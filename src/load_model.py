cac910ff4aa65b9e8b23d3abb5fa8771c11be7a5
Maksym <maxim.lupey@gmail.com>
2024-02-17 13:57:54 -0500

    new changes

--- src/load_model.py
+++ src/load_model.py
@@ -29,7 +29,7 @@ def load_model(model_type, use_icl):
         # Load a Llama model
         model_name = "TheBloke/Llama-2-7B-GGUF"
         model_path = hf_hub_download(repo_id=model_name, filename="llama-2-7b.Q4_K_M.gguf")
-        model = Llama(model_path=model_path, n_threads=1000, n_batch=5120, n_gpu_layers=640, n=128, mlock=True)
+        model = Llama(model_path=model_path, n_threads=10, n_batch=512, n_gpu_layers=64, n=128, mlock=True)
         if use_icl:
             return prepare_icl(model, model_type)
         return model
