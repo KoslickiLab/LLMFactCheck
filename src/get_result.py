05601444670175ea5d12d894250de7325bd19d73
Maksym <maxim.lupey@gmail.com>
2024-02-18 16:17:59 -0500

    LLMFactCheck

--- src/get_result.py
+++ src/get_result.py
@@ -24,8 +24,9 @@ def get_result(model_info, prompt, model_type):
             model = model_info
             full_prompt = prompt
         prompt = full_prompt
-        prompt_template=f'''SYSTEM: Read carefully the triple and the sentense and answer only Yes or No.
-
+        prompt_template=f'''SYSTEM: Read carefully the question, triple and the sentense and answer only Yes or No.
+Context: {context}
+        
 USER: {prompt}
 
 ASSISTANT:
