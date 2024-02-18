c3833c4c0fa6297ee353048e62077576c0f89e65
Maksym <maxim.lupey@gmail.com>
2024-02-17 21:45:19 -0500

    LLMFactCheck

--- src/load_model.py
+++ src/load_model.py
@@ -28,7 +28,7 @@ def load_model(model_type, use_icl):
     if model_type == 'llama':
         # Load a Llama model
         model_name = "TheBloke/Llama-2-70B-Chat-GGUF"
-        model_path = hf_hub_download(repo_id=model_name, filename="llama-2-70b-chat.Q2_K.gguf")
+        model_path = hf_hub_download(repo_id=model_name, filename="llama-2-70b-chat.Q5_K_S.gguf")
         model = Llama(model_path=model_path, n_threads=10, n_batch=512, n_gpu_layers=64, n=128, mlock=True)
         if use_icl:
             return prepare_icl(model, model_type)
