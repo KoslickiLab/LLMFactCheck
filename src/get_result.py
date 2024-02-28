f8123c3b94f0f53f5ab2ef810e52c020c4a1e8d8
Maksym <maxim.lupey@gmail.com>
2024-02-28 18:35:31 -0500

    LLMFactCheck

--- src/get_result.py
+++ src/get_result.py
@@ -37,7 +37,7 @@ def get_result(model_info, prompt, model_type):
         for chunk in prompt_chunks:
             # Interact with the Llama model
             print(chunk)
-            response = model(prompt=chunk, max_tokens=256, temperature=0.01,
+            response = model(prompt=chunk, max_tokens=256, temperature=0.8,
                              top_p=0.95, repeat_penalty=1.2, top_k=150, echo=False)
             result_text += response["choices"][0]["text"]
             print(result_text)
