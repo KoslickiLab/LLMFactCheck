019e56e267a2a6c24807870d93924a17697b3a2e
Maksym Lupei <maxim.lupey@gmail.com>
2024-07-27 18:20:44 -0400

    72% mixtral human labeled 1000

--- src/get_result.py
+++ src/get_result.py
@@ -25,9 +25,36 @@ def get_result(model_info, prompt, model_type):
             model = model_info
             full_prompt = prompt
         prompt = full_prompt
-        prompt_template=f'''SYSTEM: You are a computational biologist tasked with evaluating scientific claims. Your role requires you to apply critical thinking and your expertise to interpret data and research findings accurately. When responding, please start with 'Yes' or 'No' to directly address the query posed. Follow this with a comprehensive justification of your decision, integrating relevant scientific knowledge, the specifics of the case at hand, and any potential implications or nuances that may influence the interpretation of the evidence provided.            
+        prompt_template=f'''
+        Context: 
+	USER: ('\'Is the triple "Phase related to Follicle stimulating hormone measurement" directly or indirectly supported by the sentence: "In pre-menopause healthy females, blood was sampled weekly during one menstruation cycle and menstruation phases (follicular, ovulatory, luteal) were determined by FSH/LH levels."?',)
 
-        USER: {prompt}
+        ASSISTANT: Yes
+
+        USER: ('\'Is the triple "Phase related to Sodium measurement" directly or indirectly supported by the sentence: "Based on a biophysical photoreceptor model, the Na(+)- and Ca(2+)-currents and concentration changes were determined from the first transient depolarization phase of the photoreceptor response."?',)
+
+        ASSISTANT:Yes
+
+        USER: ('\'Is the triple "Phase related to Bronchoalveolar Lavage" directly or indirectly supported by the sentence: "Challenge of the airways of sensitized guinea pigs with aerosolized ovalbumin resulted in an early phase of microvascular protein leakage and a delayed phase of eosinophil accumulation in the airway lumen, as measured using bronchoalveolar lavage (BAL)."?',)
+
+        ASSISTANT: Yes
+
+        USER: ('\'Does the phrase "Ciprofloxacin related to DNA Gyrase" receive at least indirect support from the statement: "Effect of ranolazine in preventing postoperative atrial fibrillation in patients undergoing coronary revascularization surgery."?',)
+
+        ASSISTANT: No
+
+        USER: ('\'Does the phrase "Ciprofloxacin related to Crohn disease" receive at least indirect support from the statement: "Recent evidence of beneficial effects of ranolazine (RAN) in type II diabetes motivates interest in the role of the late sodium current (INaL) in glucose-stimulated insulin secretion."?',)
+
+        ASSISTANT: No
+
+        USER: ('\'Does the phrase "Ciprofloxacin related to endophthalmitis" receive at least indirect support from the statement: "Furthermore, the activated Akt/mTOR signaling pathway induced by AF was further activated by ranolazine."?',)
+
+        ASSISTANT: No
+
+
+        SYSTEM: You are a computational biologist tasked with evaluating scientific claims. Your role requires you to apply critical thinking and your expertise to interpret data and research findings accurately. Answer 'Yes' or 'No' to directly address the query posed.                 
+        
+        USER: {prompt}.
 
         ASSISTANT:
 
@@ -37,7 +64,7 @@ def get_result(model_info, prompt, model_type):
         for chunk in prompt_chunks:
             # Interact with the Llama model
             print(chunk)
-            response = model(prompt=chunk, max_tokens=256, temperature=0.8,
+            response = model(prompt=chunk, max_tokens=1, temperature=0.8,
                              top_p=0.95, repeat_penalty=1.2, top_k=150, echo=False)
             result_text += response["choices"][0]["text"]
             print(result_text)
